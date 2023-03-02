f10e03d4ee9d953101dfd2c3539556d274c5a5da
Adejumo Adeyemi <93265896+Ade-yem@users.noreply.github.com>
2023-03-02 01:27:22 +0100

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,11 +18,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
+	new->right = parent->right;
 	if (parent->right != NULL)
-	{
-		new->right = parent->right;
 		parent->right->parent = new;
-	}
+
 	parent->right = new;
 	return (new);
 }
